dc4b2f3fc287ac88f856f69acec649cfc1f89285
Leo Stelon <leostelon17@gmail.com>
2020-09-07 16:05:00 +0530

    init

---
+++ android/app/src/main/kotlin/com/sumit/dataviv/e_commerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sumit.dataviv.e_commerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/providers/homepageProvider.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce/providers/Inspector.dart';
+import 'package:http/http.dart' as http;
+
+class homepageProvider {
+  Future<dynamic> getHomePageContent() async {
+    final http.Response response =
+        await http.get(Inspector.baseAPIUrl + "/HomePageContentAPI/", headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    });
+    return response;
+  }
+
+  Future<dynamic> searchProducts(searchTerm) async {
+    final http.Response response = await http.get(
+        Inspector.baseAPIUrl + "/ProductSearchAPI/?search=" + searchTerm,
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        });
+    return response;
+  }
+}
